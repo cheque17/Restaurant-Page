51f727835431c559bdcbd9d187928d72201b94c7
cheque17 <ezequielperezrojas1999@users.noreply.github.com>
2023-08-22 21:32:39 -0600

    Add dist/ and src/

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+import Icon from './bkg-img.jpg';
+
+
+
+console.log('Today is: ' + new Date());
+
+/*const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);*/
